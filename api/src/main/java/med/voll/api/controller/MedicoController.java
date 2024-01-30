5e0570c2048f420c197233e4afb5e97cd6d02ef2
Franklin Guevara <franklingueflo@hotmail.com>
2024-01-29 20:05:12 -0500

    Nuevas actualizaciones

---
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,31 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListadoMedico;
+import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+    @Autowired
+    private MedicoRepository medicoRepository;
+    @PostMapping("/registrar-medico")
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
+        medicoRepository.save(new Medico(datosRegistroMedico));
+    }
+
+    @GetMapping("/listar-medicos")
+    public Page<DatosListadoMedico> listadoMedico(@PageableDefault(size = 2) Pageable paginacion){
+        return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
+    }
+
+}
---
+++ api/src/main/java/med/voll/api/medico/Especialidad.java
@@ -0,0 +1,8 @@
+package med.voll.api.medico;
+
+public enum Especialidad {
+    ORTOPEDIA,
+    CADIOLOGIA,
+    GINECOLOGIA,
+    PEDIATRIA
+}
